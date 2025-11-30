ff3dba79d6f589f14f27b7a4d8afd9beaea1cb1a
Jan Catrysse <23419642+jcatrysse@users.noreply.github.com>
2025-11-30 14:38:32 +0100

    Defect: dynamic :action segment in a route is deprecated

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,20 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 RedmineApp::Application.routes.draw do
-  match "projects/:id/wiki_extensions/stylesheet", to: "wiki_extensions#stylesheet", via: [:get, :post], as: "wiki_extensions_stylesheet"
-  match "projects/:id/wiki_extensions/:action", :controller => "wiki_extensions", :via => [:get, :post]
-  match "projects/:id/wiki_extensions_settings/:action", :controller => "wiki_extensions_settings", :via => [:get, :post, :put, :patch]
-  match "/wiki_extentions/emoticon/:icon_name", :controller => "wiki_extensions", :action => "emoticon", :via => [:get], :as => "wiki_extensions_emoticon"
+  scope 'projects/:id', as: 'project' do
+    match 'wiki_extensions/stylesheet', to: 'wiki_extensions#stylesheet', via: [:get, :post], as: 'wiki_extensions_stylesheet'
+
+    get    'wiki_extensions/show_vote',     to: 'wiki_extensions#show_vote'
+    post   'wiki_extensions/vote',          to: 'wiki_extensions#vote'
+    post   'wiki_extensions/add_comment',   to: 'wiki_extensions#add_comment'
+    post   'wiki_extensions/reply_comment', to: 'wiki_extensions#reply_comment'
+    match  'wiki_extensions/destroy_comment', to: 'wiki_extensions#destroy_comment', via: [:get, :delete]
+    match  'wiki_extensions/update_comment',  to: 'wiki_extensions#update_comment',  via: [:post, :patch]
+    get    'wiki_extensions/forward_wiki_page', to: 'wiki_extensions#forward_wiki_page'
+    get    'wiki_extensions/show_comments',     to: 'wiki_extensions#show_comments'
+    get    'wiki_extensions/tag',               to: 'wiki_extensions#tag'
+
+    get    'wiki_extensions_settings', to: 'wiki_extensions_settings#show',   as: 'wiki_extensions_settings'
+    match  'wiki_extensions_settings', to: 'wiki_extensions_settings#update', via: [:post, :put, :patch]
+  end
 end
